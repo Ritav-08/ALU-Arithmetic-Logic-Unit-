e44ce4d19cb1dcaacaa4f4d733b7ac8ee0224a08
Ritav <ritav08092004@gmail.com>
2026-04-17 16:20:49 +0530

    Update Design.v

--- Design.v
+++ Design.v
@@ -1,50 +1,48 @@
+//Problem 15: Design an ALU (Arithmetic Logic Unit) with 4 operations.
 `timescale 1ns / 1ps
 
-//4 ops: Addition, Subtraction, AND, OR
-module ALU4ops(
-    input [31:0]a_i, //A
-    input [31:0]b_i, //B
-    input [1:0]sel_i, //S0 S1
-    output reg [31:0]dout_o, //D
+module ALU4ops( 
+    input [31:0]a_i, //A, first input
+    input [31:0]b_i, //B, Second input
+    input [1:0]sel_i, //S1 S0, Select line
+    output reg [31:0]dout_o, //D, data Output
     output reg fZero_o, //Zero flag
-    output reg fCarry_o, //Carry Flag (no borrow, adder carry)
+    output reg fCarry_o, //Carry Flag (High -> No borrow in Subtraction or Carry in Addition)
     output reg fOverflow_o, //Overflow Flag
-    output reg fNegative_o //Negative Flag
+    output reg fNegative_o //Negative ALU result Flag
 );
 
 //net(s)
-wire [31:0]sum_o; //A+B
-wire Acarry_o; //Carry of A+B
-wire [31:0]diff_o; //A-B = A + B's 2s comp
-wire Scarry_o; //Carry of [A + B's 2s comp])
-wire [31:0]and_o; //parallel AND
-wire [31:0]or_o; //-- OR
+wire [31:0]sum_o; //Sum of Adder
+wire Acarry_o; //Carry of Adder
+wire [31:0]diff_o; //Result of Subtraction via 2s comp method
+wire Scarry_o; //Carry of Addition in 2s comp Subtraction method
+wire [31:0]and_o; //Result of 32 parallel ANDs
+wire [31:0]or_o; //Result of 32 parallel ORs
 
 //Addition
 assign {Acarry_o, sum_o} = a_i + b_i;
-
 //Subtraction
-assign {Scarry_o, diff_o} = a_i + (~b_i) + 1; //A + B's 2s comp
-
+assign {Scarry_o, diff_o} = a_i + (~b_i) + 1;
 //logical AND
 assign and_o = a_i & b_i;
-
 //logical OR
 assign or_o = a_i | b_i;
 
 //ALU Output selection
-always@(*) begin
-//default outputs
+always@(a_i, 
+   b_i, 
+   sel_i) begin
+    
+//initialization -- Reset ALU outputs -- 
 dout_o = 32'b0;
-//fZero_o = 1'b0; //going to calculate anyway
 fCarry_o = 1'b0;
 fOverflow_o = 1'b0;
-//fNegative_o = 1'b0; //going to calculate anyway
-//MUX
+    
+//MUX -- selecting ALU output
 case (sel_i)
-//Addition
-2'b00: begin 
-   dout_o = sum_o;
+2'b00: begin //Addition (00)
+   dout_o = sum_o; 
    fCarry_o = Acarry_o; //Carry Flag = Carry of Adder
    fOverflow_o = (a_i[31] & b_i[31] & ~sum_o[31]) | (~a_i[31] & ~b_i[31] & sum_o[31]); //P+P=N or N+N=P ? (P -> +ve, N -> -ve)
 end
